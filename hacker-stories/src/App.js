9a4dcba24d0034019b07f62f20e9141ca75fa70a
Moses-Onche <onchemoses38@gmail.com>
2024-12-05 15:42:50 +0100

    Build a component to use the list

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,3 +1,21 @@
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    numOfComments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clarke",
+    numOfComments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
 
 function App() {
 
@@ -5,25 +23,6 @@ function App() {
   //   greeting: "Hey",
   //   title: "React",
   // }
-
-  const list = [
-    {
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Jordan Walke",
-      numOfComments: 3,
-      points: 4,
-      objectID: 0
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org",
-      author: "Dan Abramov, Andrew Clarke",
-      numOfComments: 2,
-      points: 5,
-      objectID: 1
-    }
-  ];
 
   return (
     <div className="App">
@@ -46,7 +45,28 @@ function App() {
           </li>
           })}
       </ul>
+    <List />
+    </div>
+  );
+}
 
+const List = () => {
+  return (
+    <div>
+      <ul>
+        {list.map(item => {
+          return (
+            <li key={item.objectID} >
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.numOfComments}</span>
+              <span>{item.points}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
