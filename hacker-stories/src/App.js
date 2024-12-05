45460d167737e94e22c22aa141616534bdfc3819
Moses-Onche <onchemoses38@gmail.com>
2024-12-05 15:30:48 +0100

    Use lists in react to populate html attributes and content

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,16 +1,52 @@
 
 function App() {
 
-  const welcome = {
-    greeting: "Hey",
-    title: "React",
-  }
+  // const welcome = {
+  //   greeting: "Hey",
+  //   title: "React",
+  // }
+
+  const list = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      numOfComments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clarke",
+      numOfComments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+
   return (
     <div className="App">
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search</label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map((item) => {
+          return <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.numOfComments}</span>
+            <span>{item.points}</span>
+          </li>
+          })}
+      </ul>
+
     </div>
   );
 }
