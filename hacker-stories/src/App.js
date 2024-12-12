dc1cc6e8abf9b4b207bab3e89753df9dad34f15b
Moses-Onche <onchemoses38@gmail.com>
2024-12-12 18:13:58 +0100

    Move list object into app component and pass it as a prop to list component

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,32 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
-  <div className="App">
-    <h1>My Hacker Stories</h1>
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      numOfComments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clarke",
+      numOfComments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
 
-    <Search />
+  return (
+    <div className="App">
+      <h1>My Hacker Stories</h1>
 
-    <hr />
+      <Search />
 
-    <ul>
-      {list.map((item) => {
-        return <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.numOfComments}</span>
-          <span>{item.points}</span>
-        </li>
-        })}
-    </ul>
-    <List />
-    <MyButton />
-    <MyButton />
-  </div>
+      <hr />
+
+      <List list={stories}/>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
 };
 
-const List = () => (
+const List = ({ list }) => (
   <div>
     <ul>
       {list.map(item => (
